dcc7a727c7aa0236bd645f728566cfede4c0be82
Gustavo Henrique <gustavohgustavo@hotmail.com>
2024-02-05 10:57:28 -0300

    feature: seeding database

--- DsMovieFlix/src/main/java/com/gustavonascimento/DsMovieFlix/entities/Movie.java
+++ DsMovieFlix/src/main/java/com/gustavonascimento/DsMovieFlix/entities/Movie.java
@@ -29,6 +29,7 @@ public class Movie implements Serializable {
 	private Integer year;
 	private String subTitle;
 	private String imgUrl;
+	@Column(columnDefinition = "TEXT")
 	private String synopsis;
 	@ManyToOne
 	@JoinColumn(name = "genre_id")
