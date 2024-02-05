b738bd44233340eb2b365e5c89a1150ef8951af7
Gustavo Henrique <gustavohgustavo@hotmail.com>
2024-02-05 19:32:36 -0300

    feature: Documenting the Genre Controller.

--- DsMovieFlix/src/main/java/com/gustavonascimento/DsMovieFlix/controllers/GenreController.java
+++ DsMovieFlix/src/main/java/com/gustavonascimento/DsMovieFlix/controllers/GenreController.java
@@ -12,16 +12,27 @@ import org.springframework.web.bind.annotation.RestController;
 import com.gustavonascimento.DsMovieFlix.entities.dto.GenreDTO;
 import com.gustavonascimento.DsMovieFlix.services.GenreService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
 @RestController
 @RequestMapping(value = "/genres")
 public class GenreController {
 
 	@Autowired
 	private GenreService service;
-	
+
+	@Operation(description = "Find all a list of genres", summary = "Find all genres", responses = {
+			@ApiResponse(description = "Ok", responseCode = "200", content = @Content(mediaType = "application/json",array = @ArraySchema(schema = @Schema(implementation = GenreDTO.class)))),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content(mediaType = "application/json", examples = @ExampleObject(value = "{\n\"message\": \"Unauthorized\"}"))),
+			@ApiResponse(description = "Forbidden", responseCode = "403", content = @Content(mediaType = "application/json", examples = @ExampleObject(value = "{\n\"message\": \"Forbidden\"}")))})
 	@PreAuthorize("hasAnyRole('VISITOR','MEMBER')")
 	@GetMapping
-	public ResponseEntity<List<GenreDTO>> findAll(){
+	public ResponseEntity<List<GenreDTO>> findAll() {
 		List<GenreDTO> entities = service.findAll();
 		return ResponseEntity.ok(entities);
 	}
