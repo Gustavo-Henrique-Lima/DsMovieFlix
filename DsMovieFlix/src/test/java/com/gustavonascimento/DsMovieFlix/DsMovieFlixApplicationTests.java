2a7b9a58a25df80c8a37eb1d32cec0ce989ec696
Gustavo Henrique <gustavohgustavo@hotmail.com>
2024-02-05 08:50:21 -0300

    Initial commit

---
+++ DsMovieFlix/src/test/java/com/gustavonascimento/DsMovieFlix/DsMovieFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavonascimento.DsMovieFlix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsMovieFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
