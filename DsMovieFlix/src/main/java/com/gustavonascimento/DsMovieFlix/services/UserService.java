6a8aa78b15712db5bbd68eed33541189c7f9bd07
Gustavo Henrique <gustavohgustavo@hotmail.com>
2024-02-05 09:00:03 -0300

    feature: Created the User class and its layers.

---
+++ DsMovieFlix/src/main/java/com/gustavonascimento/DsMovieFlix/services/UserService.java
@@ -0,0 +1,27 @@
+package com.gustavonascimento.DsMovieFlix.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.gustavonascimento.DsMovieFlix.entities.User;
+import com.gustavonascimento.DsMovieFlix.repositories.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository repository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User entity = repository.findByEmail(username);
+		if (entity == null) {
+			throw new UsernameNotFoundException("Email não encontrado");
+		}
+		return entity;
+	}
+
+}
