86f76f0dd3560c7684c236f2f8d88bb28c686bc9
Gustavo Henrique <gustavohgustavo@hotmail.com>
2024-02-05 21:35:07 -0300

    feature: Documenting the Movie Controller and Correcting the Genre Controller Documentation.

--- DsMovieFlix/src/main/java/com/gustavonascimento/DsMovieFlix/controllers/MovieController.java
+++ DsMovieFlix/src/main/java/com/gustavonascimento/DsMovieFlix/controllers/MovieController.java
@@ -12,8 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gustavonascimento.DsMovieFlix.controllers.exceptions.StandardError;
 import com.gustavonascimento.DsMovieFlix.entities.dto.MovieDTO;
 import com.gustavonascimento.DsMovieFlix.services.MovieService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 @RestController
 @RequestMapping(value = "/movies")
@@ -22,6 +29,10 @@ public class MovieController {
 	@Autowired
 	private MovieService service;
 
+	@Operation(description = "Find all a page of movies", summary = "Find all movies", responses = {
+			@ApiResponse(description = "Ok", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Page.class, type = "Page<MovieDTO>"))),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content(mediaType = "application/json", examples = @ExampleObject(value = "{\n\"message\": \"Unauthorized\"}"))),
+			@ApiResponse(description = "Forbidden", responseCode = "403", content = @Content(mediaType = "application/json", examples = @ExampleObject(value = "{\n\"message\": \"Forbidden\"}")))})
 	@PreAuthorize("hasAnyRole('VISITOR','MEMBER')")
 	@GetMapping
 	public ResponseEntity<Page<MovieDTO>> findAll(@RequestParam(value = "genreId", defaultValue = "0") Long genreId,
@@ -30,10 +41,24 @@ public class MovieController {
 			@RequestParam(value = "orderBy", defaultValue = "title") String orderBy,
 			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		Page<MovieDTO> entity = service.findAll(genreId,pageRequest);
+		Page<MovieDTO> entity = service.findAll(genreId, pageRequest);
 		return ResponseEntity.ok(entity);
 	}
-	
+
+	@Operation(description = "Find by id", summary = "Return a movie when id exist", responses = {
+			@ApiResponse(description = "Ok", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MovieDTO.class))),
+			@ApiResponse(
+				    description = "Not Found",
+				    responseCode = "404",
+				    content = @Content(
+				        mediaType = "application/json",
+				        examples = @ExampleObject(value = "{\n\"timestamp\": \"2022-01-01T12:00:00Z\",\n\"status\": 404,\n\"error\": \"Recurso não encontrado\",\n\"message\": \"Filme não encontrado\",\n\"path\": \"/movies/{id}\"}"
+				        ),
+				        schema = @Schema(implementation = StandardError.class)
+				    )
+				),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content(mediaType = "application/json", examples = @ExampleObject(value = "{\n\"message\": \"Unauthorized\"}"))),
+			@ApiResponse(description = "Forbidden", responseCode = "403", content = @Content(mediaType = "application/json", examples = @ExampleObject(value = "{\n\"message\": \"Forbidden\"}"))) })
 	@PreAuthorize("hasAnyRole('VISITOR','MEMBER')")
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<MovieDTO> findById(@PathVariable Long id) {
