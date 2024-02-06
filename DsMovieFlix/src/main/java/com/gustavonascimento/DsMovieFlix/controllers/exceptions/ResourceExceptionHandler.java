d08c45cee9f69099df196e7bb1b4819348f08158
Gustavo Henrique <gustavohgustavo@hotmail.com>
2024-02-06 19:33:30 -0300

    feature: Solved half of the tests in the Review class.

--- DsMovieFlix/src/main/java/com/gustavonascimento/DsMovieFlix/controllers/exceptions/ResourceExceptionHandler.java
+++ DsMovieFlix/src/main/java/com/gustavonascimento/DsMovieFlix/controllers/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -23,5 +25,19 @@ public class ResourceExceptionHandler {
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(err);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		ValidationError err = new ValidationError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(HttpStatus.UNPROCESSABLE_ENTITY.value());
+		err.setError("Exceção de validação");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		for(FieldError f: e.getBindingResult().getFieldErrors()) {
+			err.addError(f.getField(), f.getDefaultMessage());
+		}
+		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY.value()).body(err);
 	}
 }
