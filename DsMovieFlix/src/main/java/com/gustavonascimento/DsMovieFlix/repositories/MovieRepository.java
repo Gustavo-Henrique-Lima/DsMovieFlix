4ad5c8e599f98f71b6c93ac47deea3ab15fbae9e
Gustavo Henrique <gustavohgustavo@hotmail.com>
2024-02-05 21:10:29 -0300

    feature: Solving the Movie Class Tests.

--- DsMovieFlix/src/main/java/com/gustavonascimento/DsMovieFlix/repositories/MovieRepository.java
+++ DsMovieFlix/src/main/java/com/gustavonascimento/DsMovieFlix/repositories/MovieRepository.java
@@ -1,11 +1,26 @@
 package com.gustavonascimento.DsMovieFlix.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.gustavonascimento.DsMovieFlix.entities.Genre;
 import com.gustavonascimento.DsMovieFlix.entities.Movie;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
-
+	
+	@Query("SELECT DISTINCT obj FROM Movie obj "
+	        + "INNER JOIN obj.genre gen "
+	        + "WHERE (:genres IS NULL OR gen IN (:genres))")
+	Page<Movie> find(List<Genre> genres, Pageable pageable);
+	
+	@Query("SELECT obj FROM Movie obj "
+	        + "INNER JOIN FETCH obj.genre gen "
+	        + "WHERE obj IN :movies")
+	List<Movie> findMoviesWithGenre(List<Movie> movies);
 }
--- DsMovieFlix/src/main/java/com/gustavonascimento/DsMovieFlix/services/MovieService.java
+++ DsMovieFlix/src/main/java/com/gustavonascimento/DsMovieFlix/services/MovieService.java
@@ -1,22 +1,44 @@
 package com.gustavonascimento.DsMovieFlix.services;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.gustavonascimento.DsMovieFlix.entities.Genre;
 import com.gustavonascimento.DsMovieFlix.entities.Movie;
+import com.gustavonascimento.DsMovieFlix.entities.dto.GenreDTO;
+import com.gustavonascimento.DsMovieFlix.entities.dto.MovieDTO;
+import com.gustavonascimento.DsMovieFlix.repositories.GenreRepository;
 import com.gustavonascimento.DsMovieFlix.repositories.MovieRepository;
+import com.gustavonascimento.DsMovieFlix.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class MovieService {
 
 	@Autowired
 	private MovieRepository repository;
+
+	@Autowired
+	private GenreRepository genreRepository;
+
+	@Transactional(readOnly = true)
+	public Page<MovieDTO> findAll(Long genreId, PageRequest page) {
+		List<Genre> genres = (genreId == 0) ? null : Arrays.asList(genreRepository.getReferenceById(genreId));
+		Page<Movie> entities = repository.find(genres, page);
+		Page<MovieDTO> entitiesDto = entities.map(movie -> new MovieDTO(movie, new GenreDTO(movie.getGenre())));
+		return entitiesDto;
+	}
 	
 	@Transactional(readOnly = true)
-	public List<Movie> findAll(){
-		return repository.findAll();
+	public MovieDTO findById(Long id) {
+		Optional<Movie> obj = repository.findById(id);
+		Movie entity = obj.orElseThrow(() -> new ResourceNotFoundException("Filme não encontrado"));
+		return new MovieDTO(entity,new GenreDTO(entity.getGenre()));
 	}
 }
